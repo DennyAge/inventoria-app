813324b8000b39a5d466159e52acf93d56d0da41
DennyAge <DenX055@gmail.com>
2025-01-20 17:47:26 +0200

    add error page

---
+++ client/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="error_page">
+    <div class="error_page__content" v-if="error?.statusCode === 500">
+      <Image src="/images/500.jpg" width="50%" />
+      <span>Server error! Please try later...</span>
+    </div>
+    <div class="error_page__content" v-if="error?.statusCode === 404">
+      <Image src="/images/404.jpg" width="50%" />
+      <button class="btn btn-primary" @click="goToDashboard">
+        Go to Dashboard
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const goToDashboard = () => clearError({ redirect: "/" });
+</script>
+
+<style scoped>
+.error_page {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.error_page__content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
