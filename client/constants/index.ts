408ee386fbb9167fdb2c356b88ea15e79ea4597e
DennyAge <DenX055@gmail.com>
2025-01-24 12:13:30 +0200

    add new options for selects

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,3 +1,5 @@
+import type { Guarantee } from "~/types";
+
 export const navigationLinks = [
   {
     path: "/",
@@ -15,3 +17,50 @@ export const navigationLinks = [
     icon: "lucide:monitor",
   },
 ];
+
+export const productSpecification = {
+  en: ["Display", "Performance", "Camera", "Battery", "Other Features"],
+  ru: [
+    "Экран",
+    "Производительность",
+    "Камера",
+    "Батарея",
+    "Дополнительные характеристики",
+  ],
+  ua: [
+    "Екран",
+    "Продуктивність",
+    "Камера",
+    "Батарея",
+    "Додаткові характеристики",
+  ],
+};
+export const productTypes = {
+  en: [
+    "Smartphones",
+    "Laptops",
+    "Smart watches",
+    "Wireless headphones",
+    "Tablets",
+  ],
+  ru: [
+    "Смартфоны",
+    "Ноутбуки",
+    "Смарт-часы",
+    "Беспроводные наушники",
+    "Планшеты",
+  ],
+  ua: [
+    "Смартфони",
+    "Ноутбуки",
+    "Смарт-годинник",
+    "Бездротові навушники",
+    "Планшети",
+  ],
+};
+
+export const productGuarantee = {
+  en: ["1 month", "3 month", "6 month", "1 year"],
+  ru: ["1 месяц", "3 месяца", "6 месяцев", "1 год"],
+  ua: ["1 місяць", "3 місяць", "6 місяців ", "1 рік"],
+};
