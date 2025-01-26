3005832bf7b6415611fb85bf76a1e3cafed69850
DennyAge <DenX055@gmail.com>
2025-01-26 14:15:56 +0200

    add map component

---
+++ client/components/Map.vue
@@ -0,0 +1,66 @@
+<template>
+  <div ref="mapContainer" class="map-container"></div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  locations: Array<{ lat: number; lng: number; name: string }>;
+  zoom?: number;
+}>();
+
+const mapContainer = ref<HTMLElement | null>(null);
+let map: L.Map | null = null;
+
+onMounted(async () => {
+  const L = await import("leaflet");
+  await import("leaflet/dist/leaflet.css");
+
+  if (mapContainer.value) {
+    map = L.map(mapContainer.value);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors",
+    }).addTo(map);
+
+    const bounds = L.latLngBounds(
+      props.locations.map((location) => [location.lat, location.lng]),
+    );
+    props.locations.forEach((location) => {
+      L.marker([location.lat, location.lng])
+        .addTo(map!)
+        .bindPopup(location.name)
+        .openPopup();
+    });
+
+    if (map) {
+      map.fitBounds(bounds);
+    }
+  }
+});
+
+onBeforeUnmount(() => {
+  if (map) {
+    map.remove();
+    map = null;
+  }
+});
+</script>
+
+<style scoped>
+.map-container {
+  width: 100%;
+  height: 25rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+}
+@media screen and (max-height: 1000px) {
+  .map-container {
+    height: 20rem;
+  }
+}
+@media screen and (max-height: 800px) {
+  .map-container {
+    height: 15rem;
+  }
+}
+</style>
