ee21fb764837941d4dce4e2f9c8a3bb12d85840a
DennyAge <DenX055@gmail.com>
2025-02-08 15:51:06 +0200

    tailwind migration: LanguageSelect.vue

--- client/components/LanguageSelect.vue
+++ client/components/LanguageSelect.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="language-select">
+  <div class="flex items-center justify-center gap-2">
     <button
       v-for="loc in locales"
       :key="loc.code"
-      class="language-select__bnt"
-      :class="loc.code === locale && 'active'"
+      class="border-none bg-transparent"
+      :class="loc.code === locale && 'font-bold'"
       @click="setLocale(loc.code)"
     >
       {{ loc.code.toUpperCase() }}
@@ -15,19 +15,3 @@
 <script lang="ts" setup>
 const { locale, locales, setLocale } = useI18n();
 </script>
-
-<style scoped>
-.language-select {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 0.625rem;
-}
-.language-select__bnt {
-  border: none;
-  background: transparent;
-}
-.active {
-  font-weight: bold;
-}
-</style>
