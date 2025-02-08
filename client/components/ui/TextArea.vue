58350a329f227db95322e7bc5ad9284af6b58c51
DennyAge <DenX055@gmail.com>
2025-02-08 17:53:32 +0200

    add TextArea component

---
+++ client/components/ui/TextArea.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { useVModel } from "@vueuse/core";
+
+const props = defineProps<{
+  defaultValue?: string;
+  modelValue?: string;
+  class?: HTMLAttributes["class"];
+  placeholder?: string;
+  label?: string;
+  name?: string;
+  error?: string;
+  required?: boolean;
+  readOnly?: boolean;
+  disabled?: boolean;
+  maxLength?: number;
+  rows?: string;
+}>();
+
+const emits = defineEmits<{
+  (e: "update:modelValue", payload: string | number): void;
+}>();
+
+const modelValue = useVModel(props, "modelValue", emits, {
+  defaultValue: props.defaultValue,
+});
+</script>
+
+<template>
+  <div>
+    <label v-if="label" class="text-[14px] mb-1" :for="name">{{ label }}</label>
+    <div>
+      <textarea
+        v-model="modelValue"
+        :name="name"
+        :maxlength="maxLength"
+        :required="required"
+        :disabled="disabled"
+        :readOnly="readOnly"
+        :placeholder="placeholder"
+        :rows="rows"
+        :class="
+          cn('flex p-2 w-full rounded-md border border-gray-200 bg-white', {
+            'border-red-500': error,
+          })
+        "
+        style="resize: none"
+      />
+      <span v-if="error" class="text-red-500 text-xs mb-1">
+        {{ error }}
+      </span>
+    </div>
+  </div>
+</template>
