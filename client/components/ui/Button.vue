fa016bbe6196ee082b9911633ae486afc67fd044
DennyAge <DenX055@gmail.com>
2025-02-24 21:14:54 +0200

    work on mobile-resp

--- client/components/ui/Button.vue
+++ client/components/ui/Button.vue
@@ -10,7 +10,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "~/lib/utils.js";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-md shadow-xs w-max",
+  "inline-flex items-center justify-center rounded-md text-sm md:text-base shadow-xs w-max",
   {
     variants: {
       variant: {
@@ -21,10 +21,10 @@ const buttonVariants = cva(
         icon: "",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        full: "w-full h-10 rounded-md px-8",
+        default: "h-8 md:h-10 px-4 py-2",
+        sm: "h-7 md:h-9 rounded-md px-3",
+        lg: "h-9 md:h-11 rounded-md px-8",
+        full: "w-full h-8 md:h-10 rounded-md px-8",
         icon: "w-max h-max",
       },
     },
