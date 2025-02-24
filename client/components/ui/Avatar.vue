9fc1233da7205cff7334a2f301bc49bb2425f5ca
DennyAge <DenX055@gmail.com>
2025-02-24 21:14:51 +0200

    work on mobile-resp

--- client/components/ui/Avatar.vue
+++ client/components/ui/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex flex-row items-center justify-center border-none cursor-pointer bg-primary-100 hover:bg-primary-100/70 font-bold text-white w-10 h-10 rounded-full"
+    class="flex flex-row items-center justify-center border-none cursor-pointer bg-primary-100 hover:bg-primary-100/70 text-sm md:text-base font-bold text-white w-8 md:w-10 h-8 md:h-10 rounded-full"
     @click="goToProfile"
   >
     {{ getInitials(user.fullName) || "IN" }}
